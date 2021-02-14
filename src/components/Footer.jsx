da872bffaeff920ac75b64e8307a3163d991d1f6
Jehmes <jeihmesthales@hotmail.com>
2021-02-13 22:13:00 -0300

    added CSS and Component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            App desenvolvido por Jehmes Thales
+            <div >
+                <p>© 2021 Games Studios - All rights reserved</p>
+                <span>
+                <a href="https://www.instagram.com/jehmesthales/" target='_blank'>Instagram</a> |
+               | <a href="https://www.linkedin.com/in/jehmes-thales-32b89918a/" target='_blank'>Linkedin</a> |
+               | <a href="https://github.com/jehmes" target='_blank'>Github</a>
+                </span>               
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
