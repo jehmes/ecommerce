383895e28d2eb4ad3fe8bf8fe32fa68f0a96d3ae
Jehmes <jeihmesthales@hotmail.com>
2021-02-13 22:20:35 -0300

    Upgrade component Content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SideBar from './SideBar'
+import Main from './Main'
+import Footer from './Footer'
 
 const Content = props => {
     return (
     <main>
         <SideBar/>
-       
-        
+        <Main/>
+        <Footer/>
     </main>
     )
 }
